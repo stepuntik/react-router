daa69d3eb4f4428827d13ac865a4939453d556d6
Stepan Lavrenchuk <stepuntik@gmail.com>
2021-08-19 16:43:15 +0800

    Updates

---
+++ precache-manifest.cb613de57402682caaef7bebbee456bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d89f79fcf29e8deef327",
+    "url": "/react-router/static/css/main.1c71795a.chunk.css"
+  },
+  {
+    "revision": "d89f79fcf29e8deef327",
+    "url": "/react-router/static/js/main.35ffcc51.chunk.js"
+  },
+  {
+    "revision": "c6984152ca93223c2cb4",
+    "url": "/react-router/static/js/runtime~main.fdc07815.js"
+  },
+  {
+    "revision": "34600b3f6cf8684f1502",
+    "url": "/react-router/static/js/2.b2c52e3d.chunk.js"
+  },
+  {
+    "revision": "97f8b3f1ab3e5c6d3dc5a492404e84bc",
+    "url": "/react-router/index.html"
+  }
+];
